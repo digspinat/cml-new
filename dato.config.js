9b2db9ffb26afeaceb4aff34463c42219a94928a
Mitesh <cmilan@pistalix.com>
2018-01-11 13:35:56 +0530

    Dato Manual

--- dato.config.js
+++ dato.config.js
@@ -1,3 +1,29 @@
 module.exports = (dato, root, i18n) => {
-  
+  console.log(dato.manuals);
+  root.directory("content/manuals", (dir) => {
+    dato.manuals.forEach((manuals) => {
+        dir.createPost(`${manuals.url}.md`, "yaml", {
+          frontmatter: {
+            title: manuals.manualTitle,
+            type: "manual",
+            sku: manuals.manualSku,
+            currency: manuals.manualCurrency,
+            donwloadid: manuals.manualDownloadId,
+            manualstatus: manuals.manualStatus,
+            sendowl: manuals.manualSendowl.toMap(),
+            manualdescription: manuals.manualDescription.descText,
+            manualcover: manuals.manualEngines.engTable,
+            manualcover: manuals.manualToc.tocDescription,
+            overallrate: manuals.review.revOverallRating,
+            manualreview: manuals.review.blockReview.toMap(),
+            carspec: manuals.carSpecs.toMap(),
+            manualspec: manuals.manualSpecs.toMap(),
+            addtocart: manuals.addToCart.addToCartList,
+            catimage: manuals.imageCatalogPage,
+            proimage: manuals.imageProductPage
+          },
+          content: manuals.manualTitle
+        });
+      });
+  });
 }
