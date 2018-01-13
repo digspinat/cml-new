1458c43cd83efd84823cd3cd23c5aac75f8a1f8f
Mitesh <cmilan@pistalix.com>
2018-01-13 13:43:23 +0530

    Algolia

--- dato.config.js
+++ dato.config.js
@@ -1,21 +1,24 @@
 module.exports = (dato, root, i18n) => {
-  // console.log(dato.pages);
+  console.log(dato.manuals);
   root.directory("content/manual", (dir) => {
     dato.manuals.forEach((manuals) => {
         dir.createPost(`${manuals.url}.md`, "yaml", {
           frontmatter: {
             title: manuals.manualTitle,
+            manualname: manuals.manualName,
             type: "manual",
             sku: manuals.manualSku,
             currency: manuals.manualCurrency,
             donwloadid: manuals.manualDownloadId,
             manualstatus: manuals.manualStatus,
             sendowl: manuals.manualSendowl.soString,
+            sendowlprice: manuals.manualSendowl.soPrice,
             manualdescription: manuals.manualDescription.descText,
             manualengcover: manuals.manualEngines.engTable,
             manualcover: manuals.manualToc.tocDescription,
             overallrate: manuals.review.revOverallRating,
             manualreview: manuals.review.blockReview.toMap(),
+            carspecs: manuals.carSpecs.toMap(),
             makes: manuals.carSpecs.cpMake,
             series: manuals.carSpecs.cpSerie,
             subseries: manuals.carSpecs.cpSubSerie,
@@ -51,8 +54,7 @@ module.exports = (dato, root, i18n) => {
       var brand = {};
       content['make'] = Manuals.carSpecs.cpMake;
       content['series'] = Manuals.carSpecs.cpSerie;
-      content['series'] = Manuals.carSpecs.cpSubSerie;
-      content['series'] = Manuals.carSpecs.cpSubSerie;
+      content['subseries'] = Manuals.carSpecs.cpSubSerie;
       content['platform'] = Manuals.carSpecs.cpPlatform;
       content['years'] = Manuals.carSpecs.cpYears;
       content['bodytype'] = Manuals.carSpecs.cpBodyType;
@@ -60,6 +62,8 @@ module.exports = (dato, root, i18n) => {
       content['title'] = Manuals.manualTitle;
       content['catimage'] = Manuals.imageCatalogPage;
       content['proimage'] = Manuals.imageProductPage;
+      content['url'] = Manuals.url;
+      content['manualname'] = Manuals.manualName;
       myall.push(content)
       if(j == 1){
         checkbrand.push(Manuals.carSpecs.cpMake);
